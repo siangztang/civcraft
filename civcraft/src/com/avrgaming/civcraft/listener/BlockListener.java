aa63a365017ee53f3f177baeb500a0e661745333
Your Coal <yourcoal5446@gmail.com>
2018-01-15 10:44:00 -0600

    Fix Player

--- civcraft/src/com/avrgaming/civcraft/listener/BlockListener.java
+++ civcraft/src/com/avrgaming/civcraft/listener/BlockListener.java
@@ -1025,8 +1025,8 @@ public class BlockListener implements Listener {
 		}
 
 		// prevent players trampling crops
-		Block soilBlock1 = p.getLocation().getBlock().getRelative(0, 0, 0);
-		Block soilBlock2 = p.getLocation().getBlock().getRelative(0, -1, 0);
+		Block soilBlock1 = event.getPlayer().getLocation().getBlock().getRelative(0, 0, 0);
+		Block soilBlock2 = event.getPlayer().getLocation().getBlock().getRelative(0, -1, 0);
 		if ((event.getAction() == Action.PHYSICAL)) {
 			if (ItemManager.getId(soilBlock1) == CivData.FARMLAND || ItemManager.getId(soilBlock2) == CivData.FARMLAND) {
 				event.setCancelled(true);
